af4882528331a180d0e5a40736fbedff39c43334
Utkarshhh420 <utkarshjaiswal8467@gmail.com>
2026-08-29 19:23:44 +0530

    Time: 3 ms (94.94%) | Memory: 44.7 MB (90.19%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -1,22 +1,17 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-    int count [] = new int[1001];
-    for(int i = 0;i<nums1.length;i++){
-        count[nums1[i]]++;
-    }
-    int temp[] = new int[nums2.length];
-    int ginnewala = 0;
-    for(int j = 0;j<nums2.length;j++){
-        if(count[nums2[j]] > 0){
-            temp[ginnewala] = nums2[j];
-            ginnewala++;
-            count[nums2[j]]--;
+        int res[] = new int[nums1.length];
+        int k =0;
+        for(int i = 0;i<nums1.length;i++){
+            for(int j = 0;j<nums2.length;j++){
+                if(nums1[i] == nums2[j]){
+                    res[k] = nums1[i]; 
+                    k = k+1;
+                    nums2[j] = -1;
+                    break;
+                }
+            }
         }
-    }
-    int result[] = new int[ginnewala];
-    for(int m = 0;m<ginnewala;m++){
-        result[m] = temp[m];
-    } 
-    return result;
+        return Arrays.copyOfRange(res,0,k);
     }
 }
